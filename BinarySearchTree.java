4f8710fd32183fe1b1c681d44a6845f2aee79338
Nevermeant <82971033+TJH2@users.noreply.github.com>
2023-03-02 10:47:11 -0800

    Update BinarySearchTree.java
    added header and created employee node

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,3 +1,11 @@
+// Programmers: Ben Diskin, David Rukashaza-Hancock, T. Jake Holmes, Jaspreet Khatkar
+// CS 145: Face-To-Face
+// Date: 3/2/2023
+// Assignment: Binary Search Tree
+// Reference: Chapter 17
+// Purpose: Use recursion to add/remove employees from a fictional database
+// Extra Credit:
+
 import java.util.*;
 import java.io.*;
 
@@ -9,5 +17,20 @@ public class BinarySearchTree {
 
 class Employee {
 
-} // end node class employee
+    private int employeeID;
+    private String firstName;
+    private String lastName;
+    private String occupation;
+    Employee left;
+    Employee right;
 
+    public Employee(int employeeID, String firstName, String lastName, String occupation) {
+        this.employeeID = employeeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.occupation = occupation;
+        this.left = null;
+        this.right = null;
+    }
+
+} // end node class employee
