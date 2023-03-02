033ed1695ced1cbc4c80315d6a1119acc8089bf8
Nevermeant <82971033+TJH2@users.noreply.github.com>
2023-03-02 10:29:12 -0800

    Add files via upload
    basic boilerplate

---
+++ BinarySearchTree.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+
+    } // end of main
+} // end of lab6 class
+
+class Employee {
+
+} // end node class employee
+
